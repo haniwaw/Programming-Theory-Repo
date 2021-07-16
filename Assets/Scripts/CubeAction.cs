0c06ca3f7b4be3362286ad35c3f2ccdd7d85fe51
haniwaw <kingdakara@gmail.com>
2021-07-16 16:16:34 +0900

    interface for shape
    each shape classes deriving behavior from ShapeAction classes.

---
+++ Assets/Scripts/CubeAction.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeAction : ShapeAction
+{
+    public override void Action()
+    {
+
+    }
+}
+}
